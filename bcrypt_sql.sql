ff33ada0efd247efe40e953580cbb13333e9be3b
Mironov Jury <mironoff2007@rambler.ru>
2018-06-10 08:18:05 +0300

    new

---
+++ bcrypt_sql.sql
@@ -0,0 +1,46 @@
+﻿
+--
+-- Table structure for table "users"
+--
+
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
+  "username" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "enabled" int NOT NULL,
+  PRIMARY KEY ("username")
+) ;
+
+--
+-- Inserting data for table "users"
+--
+
+INSERT INTO "users" 
+VALUES 
+('john','{noop}test123',1),
+('mary','{noop}test123',1),
+('susan','{noop}test123',1);
+
+
+
+DROP TABLE IF EXISTS "authorities";
+CREATE TABLE "authorities" (
+  "username" varchar(50) NOT NULL,
+  "authority" varchar(50) NOT NULL,
+  CONSTRAINT "authorities_idx_1" UNIQUE ("username","authority")  ,
+  CONSTRAINT "authorities_ibfk_1" FOREIGN KEY ("username") REFERENCES "users" ("username")
+
+ ) ;
+
+--
+-- Inserting data for table "authorities"
+--
+
+INSERT INTO "authorities" 
+VALUES 
+('john','ROLE_EMPLOYEE'),
+('mary','ROLE_EMPLOYEE'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_ADMIN');
+
